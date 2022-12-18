86094cbf93d97062f46693730649150d8352a143
Luke Bennett <hello@lukebennett.com.au>
2022-12-18 13:54:02 +1100

    Update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,51 +1,15 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
-const hexRgb = require('hex-rgb');
-
-const gray = {
-	50: '#fbfbfb',
-	100: '#f7f7f7',
-	200: '#ebebeb',
-	300: '#dcdcdc',
-	400: '#b2b2b2',
-	500: '#808080',
-	600: '#636363',
-	700: '#515151',
-	800: '#3f3f3f',
-	900: '#2e2e2e',
-};
-
-const pink = {
-	50: '#fff4fa',
-	100: '#ffe0f1',
-	200: '#fcc7e5',
-	300: '#faa5d3',
-	400: '#f76aaf',
-	500: '#f2308e',
-	600: '#e2006d',
-	700: '#c20c5b',
-	800: '#99154b',
-	900: '#751a3d',
-};
-
-const blue = {
-	50: '#ebf4ff',
-	100: '#e2eeff',
-	200: '#c5dbff',
-	300: '#a4c4fe',
-	400: '#749ffc',
-	500: '#3f76f8',
-	600: '#2157eb',
-	700: '#1640bd',
-	800: '#122775',
-	900: '#0c153c',
-};
 
 /**
- * @type {(hex: string, alpha: number) => number}
+ * @type {(variableName: string) => ({opacityValue} :{opacityValue: number}) => string}
  */
-function rgba(hex, alpha) {
-	const { red, green, blue } = hexRgb(hex);
-	return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+function withOpacity(variableName) {
+	return ({ opacityValue }) => {
+		if (opacityValue !== undefined) {
+			return `rgba(var(${variableName}), ${opacityValue})`;
+		}
+		return `rgb(var(${variableName}))`;
+	};
 }
 
 /** @type {import('tailwindcss').Config} */
@@ -54,29 +18,65 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				gray,
-				pink,
-				blue,
+				'gray': {
+					50: withOpacity('--color-gray-50'),
+					100: withOpacity('--color-gray-100'),
+					200: withOpacity('--color-gray-200'),
+					300: withOpacity('--color-gray-300'),
+					400: withOpacity('--color-gray-400'),
+					500: withOpacity('--color-gray-500'),
+					600: withOpacity('--color-gray-600'),
+					700: withOpacity('--color-gray-700'),
+					800: withOpacity('--color-gray-800'),
+					900: withOpacity('--color-gray-900'),
+				},
+				'pink': {
+					50: withOpacity('--color-pink-50'),
+					100: withOpacity('--color-pink-100'),
+					200: withOpacity('--color-pink-200'),
+					300: withOpacity('--color-pink-300'),
+					400: withOpacity('--color-pink-400'),
+					500: withOpacity('--color-pink-500'),
+					600: withOpacity('--color-pink-600'),
+					700: withOpacity('--color-pink-700'),
+					800: withOpacity('--color-pink-800'),
+					900: withOpacity('--color-pink-900'),
+				},
+				'blue': {
+					50: withOpacity('--color-blue-50'),
+					100: withOpacity('--color-blue-100'),
+					200: withOpacity('--color-blue-200'),
+					300: withOpacity('--color-blue-300'),
+					400: withOpacity('--color-blue-400'),
+					500: withOpacity('--color-blue-500'),
+					600: withOpacity('--color-blue-600'),
+					700: withOpacity('--color-blue-700'),
+					800: withOpacity('--color-blue-800'),
+					900: withOpacity('--color-blue-900'),
+				},
+				'brand': {
+					primary: withOpacity('--color-brand-primary'),
+					50: withOpacity('--color-brand-50'),
+					100: withOpacity('--color-brand-100'),
+					200: withOpacity('--color-brand-200'),
+					300: withOpacity('--color-brand-300'),
+					400: withOpacity('--color-brand-400'),
+					500: withOpacity('--color-brand-500'),
+					600: withOpacity('--color-brand-600'),
+					700: withOpacity('--color-brand-700'),
+					800: withOpacity('--color-brand-800'),
+					900: withOpacity('--color-brand-900'),
+				},
 				'black': '#2c2c2c',
 				'true-black': '#000',
-				'brand': {
-					'default-shadow': `${rgba(blue[400], 0.45)}`,
-					'pink': pink[600],
-					'pink-shadow': `${rgba(pink[400], 0.45)}`,
-					'blue': blue[900],
-					'blue-shadow': `${rgba(blue[700], 0.45)}`,
-				},
-				'primary': 'var(--brand-color)',
-				'primary-light': 'var(--brand-color-light)',
-				'primary-lighter': 'var(--brand-color-lighter)',
 			},
 			fontFamily: {
-        sans: ['Century Gothic', ...defaultTheme.fontFamily.sans],
-      },
+				sans: ['Century Gothic', ...defaultTheme.fontFamily.sans],
+			},
+			ringColor: {
+				brand: withOpacity('--color-button-accent'),
+			},
 		},
 	},
-	plugins: [
-		require('@tailwindcss/forms'),
-		require('@tailwindcss/typography'),
-	],
+	plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
