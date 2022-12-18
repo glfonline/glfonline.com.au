02725da8b2e0a3fd15deda2169f5b6c9eeaa8f15
Luke Bennett <hello@lukebennett.com.au>
2022-12-18 16:00:19 +1100

    Update button styles

--- app/components/design-system/button/get-button-styles.ts
+++ app/components/design-system/button/get-button-styles.ts
@@ -1,14 +1,25 @@
 import { type VariantProps, cva } from 'class-variance-authority';
 
 export const getButtonStyles = cva(
-	'inline-flex items-center font-bold uppercase border focus:outline-none focus:ring-2 focus:ring-brand focus:ring-offset-2 transition',
+	'inline-flex items-center font-bold justify-center uppercase border focus:outline-none focus:ring-2 transition duration-300',
 	{
 		variants: {
 			variant: {
-				outline:
-					'bg-transparent border-black text-black hover:bg-gray-900 hover:text-white',
-				brand: 'border-transparent bg-brand text-white',
-				neutral: 'bg-gray-900 text-white border-transparent hover:bg-gray-800',
+				outline: [
+					'bg-transparent border-black text-black',
+					'hover:bg-gray-900 hover:text-white',
+					'focus:ring-brand focus:ring-offset-2',
+				],
+				brand: [
+					'border-transparent bg-brand-primary text-white',
+					'hover:bg-white hover:text-black',
+					'focus:ring-brand focus:ring-opacity-75',
+				],
+				neutral: [
+					'bg-gray-900 text-white border-transparent',
+					'hover:bg-gray-800',
+					'focus:ring-brand focus:ring-offset-2',
+				],
 			},
 			size: {
 				small: 'px-3 h-8 text-sm',
