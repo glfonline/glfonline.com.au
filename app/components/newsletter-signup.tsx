867289cf12d2eb2a57824d5700a7b0b6ff74d143
Luke Bennett <hello@lukebennett.com.au>
2022-12-18 18:59:26 +1100

    Update NewsletterSignup

--- app/components/newsletter-signup.tsx
+++ app/components/newsletter-signup.tsx
@@ -1,82 +1,71 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { validateField } from '~/lib/form-field-error';
+
+import { Button } from './design-system/button';
+import { Field } from './design-system/field';
+import { Heading } from './design-system/heading';
+import { TextInput } from './design-system/text-input';
+
+const newsletterSchema = z.object({
+	first_name: z.string().min(1, 'First name is required'),
+	last_name: z.string().min(1, 'Last name is required'),
+	email: z.string().trim().min(1, 'Email is required').email('Invalid email'),
+});
+
+type NewsletterSchema = z.infer<typeof newsletterSchema>;
+
 export function NewsletterSignup() {
+	const form = useForm<NewsletterSchema>({
+		resolver: zodResolver(newsletterSchema),
+	});
+	const handleSubmit = form.handleSubmit((formValues) => {
+		/** @todo Send form values to the server */
+		console.log(formValues);
+	});
 	return (
 		<article className="mx-auto max-w-7xl bg-gray-100">
-			<div className="mx-auto max-w-xl px-4 py-12 text-left sm:px-6 lg:py-16 lg:px-8">
-				<h2 className="h2 text-center">Don't miss out, join the club</h2>
+			<div className="mx-auto max-w-xl px-4 py-12 sm:px-6 lg:py-16 lg:px-8">
+				<Heading level="2" className="text-center">
+					Don't miss out, join the club
+				</Heading>
 				<span className="sr-only">Sign up for our newsletter</span>
 				<form
-					method="POST"
+					onSubmit={handleSubmit}
 					name="newsletter-signup-form"
-					target="_blank"
-					className="mt-8 w-full sm:flex"
+					className="w-full py-8 sm:flex"
 				>
 					<div className="grid w-full gap-6 sm:grid-cols-4">
-						<div className="sm:col-span-2">
-							<label
-								htmlFor="newsletter-first_name"
-								className="block text-sm leading-5 text-gray-700"
-							>
-								First name
-							</label>
-							<div className="relative mt-1 shadow-sm">
-								<input
-									type="text"
-									id="newsletter-first_name"
-									name="newsletter-first_name"
-									defaultValue=""
-									required
-									className="form-input block w-full rounded-none px-4 py-3 transition duration-150 ease-in-out"
-								/>
-							</div>
-						</div>
-						<div className="sm:col-span-2">
-							<label
-								htmlFor="newsletter-last_name"
-								className="block text-sm leading-5 text-gray-700"
-							>
-								Last name
-							</label>
-							<div className="relative mt-1 shadow-sm">
-								<input
-									type="text"
-									id="newsletter-last_name"
-									name="newsletter-last_name"
-									defaultValue=""
-									required
-									className="form-input block w-full rounded-none px-4 py-3 transition duration-150 ease-in-out"
-								/>
-							</div>
-						</div>
-						<div className="sm:col-span-4 ">
-							<div className="sm:col-span-2">
-								<label
-									htmlFor="newsletter-email"
-									className="block text-sm leading-5 text-gray-700"
-								>
-									Email address
-								</label>
-								<div className="relative mt-1 shadow-sm">
-									<input
-										type="email"
-										id="newsletter-email"
-										name="newsletter-email"
-										defaultValue=""
-										required
-										pattern="^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$"
-										className="form-input block w-full rounded-none px-4 py-3 transition duration-150 ease-in-out"
-									/>
-								</div>
-							</div>
-						</div>
-						<button
-							type="submit"
-							className="focus:shadow-outline-primary relative inline-flex w-full items-center justify-center border border-transparent bg-gray-800 px-4 py-3 text-base font-bold uppercase leading-6 tracking-wide text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:border-blue-400 focus:bg-gray-600 focus:outline-none active:bg-gray-900 disabled:cursor-wait disabled:opacity-50 sm:col-span-1 sm:col-span-4 sm:w-auto"
+						<Field
+							label="First name"
+							className="sm:col-span-2"
+							{...validateField(form.formState.errors, 'first_name')}
 						>
-							<span className="">Join</span>
-						</button>
+							<TextInput {...form.register('first_name')} />
+						</Field>
+						<Field
+							label="Last name"
+							className="sm:col-span-2"
+							{...validateField(form.formState.errors, 'last_name')}
+						>
+							<TextInput {...form.register('last_name')} />
+						</Field>
+						<Field
+							label="Email address"
+							className="sm:col-span-4"
+							{...validateField(form.formState.errors, 'email')}
+						>
+							<TextInput {...form.register('email')} />
+						</Field>
+
+						<Button type="submit" variant="neutral" className="sm:col-span-4">
+							Join
+						</Button>
 					</div>
 				</form>
-				<p className="prose mt-6 text-center">
+				<p className="prose text-center">
 					* by clicking join, you agree to receive our newsletter as well as top
 					tips to improve your game
 				</p>
