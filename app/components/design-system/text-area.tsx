5199d273f54219ec8b0f74332887f4847c850e07
Luke Bennett <hello@lukebennett.com.au>
2022-12-18 17:06:34 +1100

    Update inputs

--- app/components/design-system/text-area.tsx
+++ app/components/design-system/text-area.tsx
@@ -4,15 +4,14 @@ import { forwardRef } from 'react';
 export const TextArea = forwardRef<
 	HTMLTextAreaElement,
 	React.TextareaHTMLAttributes<HTMLTextAreaElement>
->(function TextArea(props, forwardedRef) {
+>(function TextArea({ className, rows = '4', ...consumerProps }, forwardedRef) {
 	return (
 		<textarea
-			//
-			{...props}
+			{...consumerProps}
 			ref={forwardedRef}
 			className={clsx(
 				'block w-full border-gray-300 transition focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50',
-				props.className
+				className
 			)}
 		/>
 	);
--- app/components/design-system/text-input.tsx
+++ app/components/design-system/text-input.tsx
@@ -5,18 +5,21 @@ export const TextInput = forwardRef<
 	HTMLInputElement,
 	React.InputHTMLAttributes<HTMLInputElement>
 >(function TextInput(
-	props: React.InputHTMLAttributes<HTMLInputElement>,
+	{
+		className,
+		type = 'text',
+		...consumerProps
+	}: React.InputHTMLAttributes<HTMLInputElement>,
 	forwardedRef
 ) {
 	return (
 		<input
-			//
-			{...props}
+			{...consumerProps}
 			ref={forwardedRef}
 			type="text"
 			className={clsx(
 				'block h-12 w-full border-gray-300 transition focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50',
-				props.className
+				className
 			)}
 		/>
 	);
