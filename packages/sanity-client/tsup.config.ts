dd887f432877a1bf9c8714b815c599ec8015463c
Luke Bennett <hello@lukebennett.com.au>
2023-01-08 13:14:52 +1100

    Tidy fetching packages

--- packages/sanity-client/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig((options) => ({
-	entry: ['src/index.ts'],
-	splitting: false,
-	clean: !options.watch,
-	dts: true,
-	format: ['cjs', 'esm'],
-}));
