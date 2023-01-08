0e88a39700d5d596cca57d5fb37a32389b4e7a08
Luke Bennett <hello@lukebennett.com.au>
2023-01-08 12:54:43 +1100

    Add shopify-client package

---
+++ packages/shopify-client/src/shopify-client.ts
@@ -0,0 +1,30 @@
+import { type Fetcher, GraphQLErrorResult } from '@ts-gql/fetch';
+import { type DocumentNode, print } from 'graphql';
+
+const API_URL =
+	'https://golfladiesfirst.myshopify.com/api/2022-10/graphql.json';
+const ACCESS_TOKEN = '2288cabae0640a8f47933d6ed4116607';
+
+export const shopifyClient: Fetcher = (
+	operation: DocumentNode,
+	variables?: Record<string, unknown>
+) => {
+	return fetch(API_URL, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'X-Shopify-Storefront-Access-Token': ACCESS_TOKEN,
+		},
+		body: JSON.stringify({
+			query: print(operation),
+			variables,
+		}),
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			if (data.errors?.length) {
+				throw new GraphQLErrorResult(data.data, data.errors);
+			}
+			return data.data;
+		});
+};
