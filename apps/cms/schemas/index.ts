9e465792f2332011bef22febdce90fa65fb365f2
Luke Bennett <hello@lukebennett.com.au>
2023-01-15 14:17:42 +1100

    Rename pages

--- apps/cms/schemas/index.ts
+++ apps/cms/schemas/index.ts
@@ -1,5 +1,5 @@
-import { faqPage } from './documents/faq';
-import { themePage } from './documents/theme';
+import { faqPage } from './documents/faq-page';
+import { themePage } from './documents/theme-page';
 import { collectionCardItem } from './objects/collection-card-item';
 import { faqItem } from './objects/faq-item';
 import { imageWithAlt } from './objects/image-with-alt';
