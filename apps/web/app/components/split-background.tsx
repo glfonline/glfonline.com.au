b97376b7da06879da7417463fc811b7f99ff9663
Luke Bennett <hello@lukebennett.com.au>
2023-01-22 17:51:05 +1100

    Improve split background

--- apps/web/app/components/split-background.tsx
+++ apps/web/app/components/split-background.tsx
@@ -1,16 +1,21 @@
-import { Fragment } from 'react';
-
 export function SplitBackground() {
 	return (
-		<Fragment>
-			<div
-				data-theme="ladies"
-				className="bg-brand-primary after:bg-brand-primary flex-1 after:absolute after:left-0 after:h-full after:w-1/2 after:-skew-x-12 after:transform"
-			/>
-			<div
-				data-theme="mens"
-				className="bg-brand-primary after:bg-brand-primary flex-1 after:absolute after:right-0 after:h-full after:w-1/2 after:-skew-x-12 after:transform"
-			/>
-		</Fragment>
+		<div
+			aria-hidden="true"
+			className="absolute inset-0 grid grid-cols-2 overflow-hidden"
+		>
+			<div data-theme="ladies" className="bg-brand-primary" />
+			<div data-theme="mens" className="bg-brand-primary" />
+			<div className="absolute inset-0">
+				<div
+					data-theme="ladies"
+					className="bg-brand-primary after:bg-brand-primary flex-1 after:absolute after:left-0 after:h-full after:w-1/2 after:-skew-x-12 after:transform"
+				/>
+				<div
+					data-theme="mens"
+					className="bg-brand-primary after:bg-brand-primary flex-1 after:absolute after:right-0 after:h-full after:w-1/2 after:-skew-x-12 after:transform"
+				/>
+			</div>
+		</div>
 	);
 }
