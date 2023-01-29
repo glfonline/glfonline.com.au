8915524537a972a15ed6f0f22254b32b707e1047
Luke Bennett <hello@lukebennett.com.au>
2023-01-30 08:49:17 +1100

    Add Meta Pixel

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -68,6 +68,7 @@ export default function App() {
 			</head>
 			<body className="relative flex h-full flex-col">
 				<GoogleAnalytics />
+				<MetaAnalytics />
 				<LoadingProgress />
 				<PersistQueryClientProvider
 					client={queryClient}
@@ -116,4 +117,33 @@ ${gtag.trackingIds
 		(id) => `gtag('config', '${id}', {page_path: window.location.pathname});`
 	)
 	.join('\n')}
+`;
+
+function MetaAnalytics() {
+	if (process.env.NODE_ENV === 'development') return null;
+	return (
+		<Fragment>
+			<script dangerouslySetInnerHTML={{ __html: metaInitScript }} />
+			<noscript
+				dangerouslySetInnerHTML={{
+					__html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1"/>`,
+				}}
+			/>
+		</Fragment>
+	);
+}
+
+const pixelId = '145925746089592';
+
+const metaInitScript = `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${pixelId}');
+fbq('track', 'PageView');
 `;
