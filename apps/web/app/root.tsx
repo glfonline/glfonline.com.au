3b47f52f695f1e81a191e04d5b52f2844a607654
Luke Bennett <hello@lukebennett.com.au>
2023-02-12 20:28:46 +1100

    Change height of body for error pages

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -110,7 +110,7 @@ export function CatchBoundary() {
 				<Meta />
 				<Links />
 			</head>
-			<body className="relative flex min-h-fit flex-col">
+			<body className="relative flex h-full flex-col">
 				{isNotFound ? (
 					<NotFound />
 				) : (
@@ -133,7 +133,7 @@ export function ErrorBoundary({ error }: { error: Error }) {
 				<Meta />
 				<Links />
 			</head>
-			<body className="relative flex min-h-fit flex-col">
+			<body className="relative flex h-full flex-col">
 				<GenericError error={error} />
 				<Scripts />
 			</body>
