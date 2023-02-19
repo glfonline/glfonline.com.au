4278cd09f436c1f871285123b7c41ac945d44a2f
Luke Bennett <hello@lukebennett.com.au>
2023-02-20 08:59:12 +1100

    Make filter sidebar sticky

--- apps/web/app/routes/$theme.collections.$collection.tsx
+++ apps/web/app/routes/$theme.collections.$collection.tsx
@@ -199,26 +199,28 @@ function Filters({
 	const id = useId();
 
 	return (
-		<aside>
-			<h2 className="sr-only" id={id}>
-				Filters
-			</h2>
+		<aside className="relative">
+			<div className="sticky top-40">
+				<h2 className="sr-only" id={id}>
+					Filters
+				</h2>
 
-			<button
-				aria-labelledby={id}
-				className="inline-flex items-center lg:hidden"
-				onClick={() => setMobileFiltersOpen(true)}
-				type="button"
-			>
-				<span className="text-sm font-medium text-gray-700">Filters</span>
-				<PlusIcon
-					aria-hidden="true"
-					className="ml-1 h-5 w-5 flex-shrink-0 text-gray-400"
-				/>
-			</button>
+				<button
+					aria-labelledby={id}
+					className="inline-flex items-center lg:hidden"
+					onClick={() => setMobileFiltersOpen(true)}
+					type="button"
+				>
+					<span className="text-sm font-medium text-gray-700">Filters</span>
+					<PlusIcon
+						aria-hidden="true"
+						className="ml-1 h-5 w-5 flex-shrink-0 text-gray-400"
+					/>
+				</button>
 
-			<div className="hidden lg:block">
-				<DisplayOptions />
+				<div className="hidden lg:block">
+					<DisplayOptions />
+				</div>
 			</div>
 		</aside>
 	);
