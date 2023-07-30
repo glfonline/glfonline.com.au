b63b1647ab630a53e3bafe40671415fa4c06a74c
Luke Bennett <hello@lukebennett.com.au>
2023-07-30 12:49:54 +1000

    Update to latest version of Storefront API

--- packages/shopify-client/src/shopify-client.ts
+++ packages/shopify-client/src/shopify-client.ts
@@ -2,7 +2,7 @@ import { GraphQLErrorResult, type Fetcher } from '@ts-gql/fetch';
 import { print, type DocumentNode } from 'graphql';
 
 const API_URL =
-	'https://golfladiesfirst.myshopify.com/api/2023-01/graphql.json';
+	'https://golfladiesfirst.myshopify.com/api/2023-07/graphql.json';
 const ACCESS_TOKEN = '2288cabae0640a8f47933d6ed4116607';
 
 export const shopifyClient: Fetcher = (
