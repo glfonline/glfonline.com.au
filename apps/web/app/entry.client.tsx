ca7655beb2b303e6ea5560457fc028a3fb5023dc
Luke Bennett <hello@lukebennett.com.au>
2024-01-01 09:51:22 +1100

    Add session replays

--- apps/web/app/entry.client.tsx
+++ apps/web/app/entry.client.tsx
@@ -14,7 +14,17 @@ import { SENTRY_DSN } from './lib/constants';
 Sentry.init({
 	dsn: SENTRY_DSN,
 	tracesSampleRate: 1,
+
+	// This sets the sample rate to be 10%. You may want this to be 100% while
+	// in development and sample at a lower rate in production
+	replaysSessionSampleRate: 0.1,
+
+	// If the entire session is not sampled, use the below sample rate to sample
+	// sessions when an error occurs.
+	replaysOnErrorSampleRate: 1.0,
+
 	integrations: [
+		new Sentry.Replay(),
 		new Sentry.BrowserTracing({
 			routingInstrumentation: Sentry.remixRouterInstrumentation(useEffect, useLocation, useMatches),
 		}),
