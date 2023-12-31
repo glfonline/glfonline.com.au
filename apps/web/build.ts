a726091b330be99a5eeb8699b11dda9e4acb74ac
Luke Bennett <hello@lukebennett.com.au>
2024-01-01 09:03:36 +1100

    Update build script

--- apps/web/build.ts
+++ apps/web/build.ts
@@ -3,7 +3,7 @@ import 'dotenv/config';
 import { execSync } from 'node:child_process';
 
 execSync(
-	process.env.VERCEL
+	process.env.VERCEL && process.env.SENTRY_AUTH_TOKEN
 		? 'remix build --sourcemap && sentry-upload-sourcemaps --org=glf-online --project=glfonline-com-au'
 		: 'remix build',
 	{
