18f631f61b1d4bac1c94bd5bebb359f0f9fcaf22
Luke Bennett <hello@lukebennett.com.au>
2024-09-07 14:59:53 +1000

    Update algoliasearch-client-javascript monorepo to v5

--- apps/web/app/lib/algolia.ts
+++ apps/web/app/lib/algolia.ts
@@ -1,5 +1,5 @@
-import { type Hit } from '@algolia/client-search';
-import { default as algoliasearch } from 'algoliasearch/lite';
+import { type Hit, type SearchResponse } from '@algolia/client-search';
+import { liteClient } from 'algoliasearch/lite';
 
 const ALGOLIA_APP_ID = 'SF44VDBM4X';
 const ALGOLIA_SEARCH_API_KEY = '6c771cb2cc3cdbc88f093f7b76231e7d';
@@ -15,15 +15,20 @@ export async function search<TData>({ indexName, query, pageParam, hitsPerPage }
 	hits: Hit<TData>[];
 	nextPage: number | undefined;
 }> {
-	const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_SEARCH_API_KEY);
-	const index = client.initIndex(indexName);
-
-	const { hits, page, nbPages } = await index.search<TData>(query, {
-		page: pageParam ?? 0,
-		hitsPerPage,
+	const client = liteClient(ALGOLIA_APP_ID, ALGOLIA_SEARCH_API_KEY);
+	const { results } = await client.search<TData>({
+		requests: [
+			{
+				indexName,
+				query,
+				page: pageParam,
+				hitsPerPage,
+			},
+		],
 	});
-
+	const result = results[0] as SearchResponse<TData>;
+	if (!result) return { hits: [], nextPage: undefined };
+	const { hits, nbPages, page } = result;
 	const nextPage = page + 1 < nbPages ? page + 1 : undefined;
-
 	return { hits, nextPage };
 }
