1400668532b362ed389e2937a788aa9f6985d817
Luke Bennett <hello@lukebennett.com.au>
2024-09-08 15:39:57 +1000

    Fix "getAnimations is not a function" error

--- apps/web/app/components/loading-progress.tsx
+++ apps/web/app/components/loading-progress.tsx
@@ -3,34 +3,33 @@ import { clsx } from 'clsx';
 import { useEffect, useRef, useState } from 'react';
 
 export function LoadingProgress() {
-	const navigation = useNavigation();
-	const active = navigation.state !== 'idle';
-
-	const ref = useRef<HTMLDivElement>(null);
+	const elementRef = useRef<HTMLDivElement>(null);
 	const [animationComplete, setAnimationComplete] = useState(true);
 
-	useEffect(() => {
-		if (!ref.current) return;
-		if (active) setAnimationComplete(false);
-
-		Promise.allSettled(ref.current.getAnimations().map(({ finished }) => finished)).then(
-			() => !active && setAnimationComplete(true),
-		);
-	}, [active]);
-
+	const navigation = useNavigation();
+	const isActive = navigation.state !== 'idle';
 	const progress = (() => {
 		if (navigation.state === 'idle' && animationComplete) return 0;
 		if (navigation.state === 'submitting') return (4 / 12) * 100;
 		if (navigation.state === 'loading') return (10 / 12) * 100;
 		if (navigation.state === 'idle' && !animationComplete) return 100;
-
 		return 0;
 	})();
 
+	useEffect(() => {
+		(async () => {
+			if (!elementRef.current) return;
+			if (isActive) setAnimationComplete(false);
+			const animations = elementRef.current.getAnimations?.() || [];
+			await Promise.all(animations.map(({ finished }) => finished));
+			if (!isActive) setAnimationComplete(true);
+		})();
+	}, [isActive]);
+
 	return (
 		<div
-			aria-hidden={!active}
-			aria-valuetext={active ? 'Loading' : undefined}
+			aria-hidden={!isActive}
+			aria-valuetext={isActive ? 'Loading' : undefined}
 			className="fixed inset-x-0 left-0 top-0 z-50 h-1 animate-pulse"
 			role="progressbar"
 			aria-valuenow={progress}
@@ -45,7 +44,7 @@ export function LoadingProgress() {
 					navigation.state === 'loading' && 'w-10/12',
 					navigation.state === 'idle' && !animationComplete && 'w-full',
 				)}
-				ref={ref}
+				ref={elementRef}
 			/>
 		</div>
 	);
