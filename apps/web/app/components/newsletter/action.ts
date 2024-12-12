6ef07e9462dc834ffc6417aa6644bec117bc1a5c
gordon-glf <123850668+gordon-glf@users.noreply.github.com>
2024-12-13 10:36:32 +1100

    Update action.ts

--- apps/web/app/components/newsletter/action.ts
+++ apps/web/app/components/newsletter/action.ts
@@ -17,7 +17,7 @@ export async function action({ request }: ActionFunctionArgs) {
 
 		/**
 		 * Validate Cloudflare Turnstyle token server-side
-		 * @see https://developers.cloudflare.com/turnstile/get-started/server-side-validation/
+		 * @see https://developers.cloudflare.com/turnstile/get-started/server-side-validation
 		 */
 		const challengeResponse = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
 			body: JSON.stringify({
